b33327fae2ca6324c562870fc50a693028e76cac
anand <anandakkoomart@gmail.com>
2020-02-12 18:45:48 +0800

    complete P_Hamil class without qiskit QC

--- P_Hamil.py
+++ P_Hamil.py
@@ -1,10 +1,15 @@
 from sympy import *
-from sympy.core.compatibility import exec_
-from sympy.parsing.sympy_parser import parse_expr
 
 class P_Hamil:
     """ Class for converting Objective functions into Phase/Cost Hamiltonians,"""
-    
+    I = symbols('I')
+    X = []
+    Y = []
+    Z = []    
+
+    obj_exp = ""
+    Hamil_exp = ""
+    quanCir_list = []
     
 
     def __init__(self, expr_str, var):
@@ -13,6 +18,12 @@ class P_Hamil:
         # Initialize expressions and variables
         self.expression_str = expr_str
         self.variables = var
+
+        # Number of pauli objects are limited to the number of variables/qubits being used
+        for i in range(len(var)):
+            self.Z.append(symbols('Z_{}'.format(var[i][len(var[i])-1])))
+            self.Y.append(symbols('Y_{}'.format(var[i][len(var[i])-1])))
+            self.X.append(symbols('Y_{}'.format(var[i][len(var[i])-1])))
 
 
 
@@ -25,8 +36,31 @@ class P_Hamil:
 
     
     def Hamify(self):
-        self.H_exp = parse_expr(self.expression_str)
+        self.obj_exp = sympify(self.expression_str)
 
-        print(self.H_exp)
+        for i in range(len(var)):
+            s_term = (1/2)*(I - self.Z[i])
+            self.Hamil_exp = self.obj_exp.subs(self.variables[i],s_term)
+
+        self.Hamil_exp = expand(self.Hamil_exp)
+        self.Hamil_exp = self.Hamil_exp.subs(I,1)
+        coeff = self.Hamil_exp.as_coefficients_dict()
+
+        gbl_phse = coeff.get(1)
+        self.Hamil_exp = self.Hamil_exp - gbl_phse
+
+        # Convert to expression into a sympy poly to get list of monomial expression to build the QC
+        self.quanCir_list = Poly(self.Hamil_exp).monoms()
+
+    
+    def get_ObjFun(self):
+        return self.obj_exp
+
+    def get_PHamil(self):
+        return self.Hamil_exp
+
+    def get_QClist(self):
+        return self.quanCir_list
+    
 
 
